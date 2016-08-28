75124a4bf7000bdd76cf1cf8f638ec0b4ece50d6
Kellye Greene <kellye@gust.com>
2016-08-28 13:28:20 -0400

    Styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828163043) do
+ActiveRecord::Schema.define(version: 20160828165750) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "windows", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
